73085262e26534c256f111d98d20d862ffad8004
pruettmt <pruett.michaeltodd@gmail.com>
2025-04-13 17:10:06 -0500

    lets see

--- src/about.jsx
+++ src/about.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function About() {
+export default function about() {
   return (
     <div>
       <h2 ><Link to="/">About</Link> / <Link to="/docs">Docs</Link></h2>
--- src/blog.jsx
+++ src/blog.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Blog() {
+export default function blog() {
   return (
     <div>
       <h2 ><Link to="/">About</Link> / <Link to="/docs">Docs</Link></h2>
