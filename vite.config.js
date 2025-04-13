01a802948807bb2b485be0efe03b78fa4441f58e
MT Pruett <pruett.michaeltodd@email.com>
2025-04-12 19:47:23 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // 👈 must match GitHub repo name#
+  base: '/pages/', // 👈 must match GitHub repo name#
 });
